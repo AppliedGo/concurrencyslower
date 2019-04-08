cb3653db26f0cd7dcbcb8ec783e42f1c9238786f
christophberger <christophberger@users.noreply.github.com>
2019-04-08 08:17:02 +0200

    Initial commit.

---
+++ concurrencyslower.go
@@ -0,0 +1,70 @@
+/*
+<!--
+Copyright (c) 2017 Christoph Berger. Some rights reserved.
+
+Use of the text in this file is governed by a Creative Commons Attribution Non-Commercial
+Share-Alike License that can be found in the LICENSE.txt file.
+
+Use of the code in this file is governed by a BSD 3-clause license that can be found
+in the LICENSE.txt file.
+
+The source code contained in this file may import third-party source code
+whose licenses are provided in the respective license files.
+-->
+
+<!--
+NOTE: The comments in this file are NOT godoc compliant. This is not an oversight.
+
+Comments and code in this file are used for describing and explaining a particular topic to the reader. While this file is a syntactically valid Go source file, its main purpose is to get converted into a blog article. The comments were created for learning and not for code documentation.
+-->
+
++++
+title = ""
+description = ""
+author = "Christoph Berger"
+email = "chris@appliedgo.net"
+date = "2017-00-00"
+draft = "true"
+categories = [""]
+tags = ["", "", ""]
+articletypes = ["Tutorial"]
++++
+
+### Summary goes here
+
+<!--more-->
+
+## Intro goes here
+
+## The code
+*/
+
+// ## Imports and globals
+package main
+
+/*
+## How to get and run the code
+
+Step 1: `go get` the code. Note the `-d` flag that prevents auto-installing
+the binary into `$GOPATH/bin`.
+
+    go get -d github.com/appliedgo/TODO:
+
+Step 2: `cd` to the source code directory.
+
+    cd $GOPATH/src/github.com/appliedgo/TODO:
+
+Step 3. Run the binary.
+
+    go run TODO:.go
+
+
+## Odds and ends
+## Some remarks
+## Tips
+## Links
+
+
+**Happy coding!**
+
+*/
